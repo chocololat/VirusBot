169dc5ef9392cade50b4f6aceb8d8acc4b1e97b0
MesaVirep <atamatori56@gmail.com>
2022-01-04 07:35:31 +0100

    ref(pingCommand): displays ping values

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,13 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('pong'),
-    async execute(interaction) {
-        await interaction.reply('pong');
-    }
-}
+  data: new SlashCommandBuilder().setName("ping").setDescription("pong"),
+  async execute(interaction, bot) {
+    interaction.reply(
+      `**P${"o".repeat(
+        Math.min(Math.round(bot.ws.ping / 100), 1500)
+      )}ng!** (${Math.round(bot.ws.ping)}ms)`
+    );
+  },
+};
