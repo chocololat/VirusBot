6319d3ec489fbd1eb72030a6becb58214739d6b3
MesaVirep <atamatori56@gmail.com>
2022-01-04 07:41:10 +0100

    chore: prettified files

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
-const { Client, Intents, Collection } = require('discord.js');
+const { Client, Intents, Collection } = require("discord.js");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const config = require("./config.json");
-const fs = require('fs');
+const fs = require("fs");
 
-(async() => {
-    let cmds = [];
-    const cmdFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+(async () => {
+  let cmds = [];
+  const cmdFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
 
-    for (const file of cmdFiles) {
-      const cmd = require(`./commands/${file}`);
-      cmds.push(cmd.data.toJSON());
-    }
-  
-    try {
-        const rest = new REST({ version: "9" }).setToken(config.token);
-      console.log("started refreshing /cmds");
-  
-      await rest.put(Routes.applicationGuildCommands(config.clientID, "858419638743072809"), { body: cmds });
-  
-      console.log("reloaded /cmds");
-    } catch (e) {
-      console.error(e);
-    }
-  })();
+  for (const file of cmdFiles) {
+    const cmd = require(`./commands/${file}`);
+    cmds.push(cmd.data.toJSON());
+  }
+
+  try {
+    const rest = new REST({ version: "9" }).setToken(config.token);
+    console.log("started refreshing /cmds");
+
+    await rest.put(
+      Routes.applicationGuildCommands(config.clientID, "858419638743072809"),
+      { body: cmds }
+    );
+
+    console.log("reloaded /cmds");
+  } catch (e) {
+    console.error(e);
+  }
+})();
 
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 bot.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	bot.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.data.name, command);
 }
 
-bot.once('ready', () => {
-    console.log('ready');
-})
+bot.once("ready", () => {
+  console.log("ready");
+});
 
-bot.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-    const command = bot.commands.get(interaction.commandName);
+bot.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const command = bot.commands.get(interaction.commandName);
 
-    if (!command) return;
+  if (!command) return;
 
-    try {
-        await command.execute(interaction);
-    } catch (e) {
-        console.error(e);
-    }
-})
+  try {
+    await command.execute(interaction);
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 bot.login(config.token);
