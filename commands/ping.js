cbe08be782d45acdda95334ba852cbc8e3768a89
MesaVirep <atamatori56@gmail.com>
2022-01-07 03:14:23 +0100

    chore: prettify

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,10 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("pong"),
+  data: new SlashCommandBuilder().setName("ping").setDescription("Displays the latency time between the execution time of the command."),
   async execute(interaction, bot) {
     interaction.reply(
-      `**P${"o".repeat(
-        Math.min(Math.round(bot.ws.ping / 100), 1500)
-      )}ng!** (${Math.round(bot.ws.ping)}ms)`
+      `**P${"o".repeat(Math.min(Math.round(bot.ws.ping / 100), 1500))}ng!** (${Math.round(bot.ws.ping)}ms)`
     );
   },
 };
